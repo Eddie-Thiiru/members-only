b529c7fb653f524e576c7102cada8d9d7f7fb08b
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-09-11 17:43:28 +0300

    Add ESLint linter

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
+
+const mongoDb = "YOUR MONGO URL HERE";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 const app = express();
 
@@ -13,6 +23,9 @@ app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
